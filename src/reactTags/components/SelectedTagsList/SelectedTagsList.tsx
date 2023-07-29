4f22ffcc47eab535c151bcbb5a36c310bfa9cdb4
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-07-29 14:38:40 +0330

    improve selected tags styles

--- src/reactTags/components/SelectedTagsList/SelectedTagsList.tsx
+++ src/reactTags/components/SelectedTagsList/SelectedTagsList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import CloseIcon from '../../assets/icons/closeIcon';
 import { SelectedTagsListProps } from './SelectedTagsList.interface';
 
@@ -31,20 +31,21 @@ const SelectedTagsList = ({
             {selectedTags &&
                 selectedTags.map((tag: string) => (
                     <span
+                        title={tag}
                         key={tag}
                         onClick={clickHandler}
                         onInput={(e) => console.log(e)}
                         onKeyDown={keyDown}
-                        className={`w-fit py-1.5 px-2 rounded-[4px] flex items-center gap-2.5 text-[13px] cursor-default focus:border-2 focus:border-zSecondary-400 hover:scale-105 ${
+                        className={`w-fit max-w-[400px] py-1.5 px-2 rounded-[4px] flex items-center gap-2.5 text-[13px] cursor-default focus:border-2 focus:border-zSecondary-400 hover:scale-105 ${
                             theme === 'dark' ? 'bg-zDark-5' : 'bg-zGray-300'
                         } ${selectedTagClassName}`}
                     >
-                        <p dir="auto" contentEditable={contentEditable} className="text-[13px] outline-none">
+                        <p dir="auto" contentEditable={contentEditable} className="text-[13px] outline-none truncate">
                             {tag ? tag : selectedTags.filter((i) => i !== tag)}
                         </p>
 
                         <CloseIcon
-                            className={`w-3 h-auto stroke-black cursor-pointer hover:scale-125 transition-all ease-in-out hover:stroke-red-600 ${selectedTagCloseIconClass}`}
+                            className={`w-3 h-auto shrink-0 stroke-black cursor-pointer hover:scale-125 transition-all ease-in-out hover:stroke-red-600 ${selectedTagCloseIconClass}`}
                             onClick={() => setSelectedTags(selectedTags.filter((selectTag: string) => selectTag !== tag).slice(0, maxTags))}
                         />
                     </span>
