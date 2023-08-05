2a9d19a696a0e94ae5974a5c81eddde54dcb4b83
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-08-05 18:43:46 +0330

    updated useOutsideClick hook

--- src/reactTags/hooks/useOutsideClick.ts
+++ src/reactTags/hooks/useOutsideClick.ts
@@ -1,11 +1,21 @@
 import { useEffect, useRef } from 'react';
 
-export const useOutsideClick = (callback: any) => {
+export const useOutsideClick = (callback: any, input?: boolean, selectedTag?: boolean) => {
     const ref = useRef<any>();
 
     useEffect(() => {
         const handleClick = (event: any) => {
-            if (ref.current && !ref.current.contains(event.target) && !document.getElementById('input')?.contains(event.target)) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                if (input) {
+                    if (!document.getElementById('input')?.contains(event.target)) {
+                        callback();
+                    }
+                } else if (selectedTag) {
+                    if (!document.querySelector('.tagP')?.contains(event.target)) {
+                        callback();
+                    }
+                }
+            } else {
                 callback();
             }
         };
@@ -15,7 +25,7 @@ export const useOutsideClick = (callback: any) => {
         return () => {
             document.removeEventListener('click', handleClick);
         };
-    }, [callback, ref]);
+    }, [callback, input, ref, selectedTag]);
 
     return ref;
 };
