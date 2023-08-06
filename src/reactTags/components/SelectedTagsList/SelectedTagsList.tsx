14b8ab8013e1db29e7b7261325186540c465071d
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-08-06 10:08:50 +0330

    add ref for selected tag

--- src/reactTags/components/SelectedTagsList/SelectedTagsList.tsx
+++ src/reactTags/components/SelectedTagsList/SelectedTagsList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import CloseIcon from '../../assets/icons/closeIcon';
 import { SelectedTagsListProps } from './SelectedTagsList.interface';
 import Input from '../Input/Input';
@@ -25,6 +25,7 @@ const SelectedTagsList = ({
     const [contentEditable, setContentEditable] = useState(false);
     const [editedText, setEditedText] = useState('');
     const [tagIndex, setTagIndex] = useState<number>();
+    const editedTextRef = useRef<any>();
 
     useEffect(() => {
         if (mode === 'array-of-string') {
@@ -33,34 +34,35 @@ const SelectedTagsList = ({
     }, [mode]);
 
     const selectedTagsKeyDown = (e: any) => {
-        if (e.key === 'Enter' && tagIndex !== undefined && editedText.trim()) {
+        if (e.key === 'Enter' && tagIndex !== undefined) {
             setContentEditable(false);
             setSelectedTags([
                 ...new Set(
-                    [...selectedTags.slice(0, tagIndex), (selectedTags[tagIndex] = editedText.trim()), ...selectedTags.slice(tagIndex + 1)].slice(
-                        0,
-                        maxTags,
-                    ),
+                    [
+                        ...selectedTags.slice(0, tagIndex),
+                        (selectedTags[tagIndex] = editedTextRef.current.trim()),
+                        ...selectedTags.slice(tagIndex + 1),
+                    ].slice(0, maxTags),
                 ),
             ]);
 
             onChange?.([
                 ...new Set(
-                    [...selectedTags.slice(0, tagIndex), (selectedTags[tagIndex] = editedText.trim()), ...selectedTags.slice(tagIndex + 1)].slice(
-                        0,
-                        maxTags,
-                    ),
+                    [
+                        ...selectedTags.slice(0, tagIndex),
+                        (selectedTags[tagIndex] = editedTextRef.current.trim()),
+                        ...selectedTags.slice(tagIndex + 1),
+                    ].slice(0, maxTags),
                 ),
             ]);
         }
     };
 
     const handleClickOutside = () => {
-        console.log(editedText);
-        if (tagIndex !== undefined && editedText.trim()) {
+        if (tagIndex !== undefined && editedTextRef.current) {
             setSelectedTags([
                 ...new Set(
-                    [...selectedTags.slice(0, tagIndex), (selectedTags[tagIndex] = editedText.trim()), ...selectedTags.slice(tagIndex + 1)].slice(
+                    [...selectedTags.slice(0, tagIndex), (selectedTags[tagIndex] = editedTextRef.current), ...selectedTags.slice(tagIndex + 1)].slice(
                         0,
                         maxTags,
                     ),
@@ -69,7 +71,7 @@ const SelectedTagsList = ({
 
             onChange?.([
                 ...new Set(
-                    [...selectedTags.slice(0, tagIndex), (selectedTags[tagIndex] = editedText.trim()), ...selectedTags.slice(tagIndex + 1)].slice(
+                    [...selectedTags.slice(0, tagIndex), (selectedTags[tagIndex] = editedTextRef.current), ...selectedTags.slice(tagIndex + 1)].slice(
                         0,
                         maxTags,
                     ),
@@ -83,12 +85,6 @@ const SelectedTagsList = ({
     const selectedTagOnClick = (e: any) => {
         e.stopPropagation();
     };
-
-    useEffect(() => {
-        if (selectedTags) {
-            setSelectedTags(selectedTags);
-        }
-    }, [selectedTags, setSelectedTags]);
 
     return (
         <div id="selectedTags" className="tag w-full flex flex-wrap gap-2 ml-3">
@@ -122,11 +118,13 @@ const SelectedTagsList = ({
                                 if (e.currentTarget.textContent) {
                                     text = e.currentTarget.textContent;
                                     setEditedText(text);
+                                    editedTextRef.current = text;
                                 }
                             }}
                             className={`tagP text-[13px] outline-none truncate ${contentEditable && 'cursor-text'}`}
                         >
-                            {tag ? tag : selectedTags.filter((i) => i !== tag)}
+                            {/* {tag ? tag : selectedTags.filter((i) => i !== tag)} */}
+                            {tag}
                         </p>
                         <CloseIcon
                             className={`w-3 h-auto shrink-0 stroke-black cursor-pointer hover:scale-125 transition-all ease-in-out hover:stroke-red-600 ${selectedTagCloseIconClass}`}
