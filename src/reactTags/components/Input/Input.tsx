ed32808b04d4029a8ea567f509c4356280d5cbe8
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-08-08 18:05:21 +0330

    updated Input styles

--- src/reactTags/components/Input/Input.tsx
+++ src/reactTags/components/Input/Input.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import { InputProps } from './Input.interface';
 
-const Input = ({
-    theme,
-    placeholder,
-    value,
-    changeHandler,
-    keyDown,
-    inputClassName,
-    selectedTags,
-    inputFocus,
-    setInputFocus,
-    setShowDropdown,
-}: InputProps) => {
+const Input = ({ theme, placeholder, value, changeHandler, keyDown, inputClassName, inputFocus, setInputFocus, setShowDropdown }: InputProps) => {
     return (
         <input
             id="input"
@@ -25,9 +14,9 @@ const Input = ({
             autoFocus={inputFocus}
             onClick={() => setInputFocus(true)}
             onFocus={() => setShowDropdown(true)}
-            className={`flex-1 h-8 pl-4 rounded-[10px] outline-none !text-right bg-transparent text-sm placeholder:text-zSecondary-400 ${
-                selectedTags.length ? 'placeholder:opacity-' : 'placeholder:opacity-'
-            } focus:placeholder:opacity-100 transition-all ease-in-out ${theme === 'dark' ? 'text-white' : 'text-zGray-800'} ${inputClassName}`}
+            className={`flex-1 h-8 pl-4 rounded-[10px] outline-none !text-right bg-transparent placeholder:text-zSecondary-400 focus:placeholder:opacity-100 transition-all ease-in-out ${
+                theme === 'dark' ? 'text-white' : 'text-zGray-800'
+            } ${inputClassName}`}
         />
     );
 };
